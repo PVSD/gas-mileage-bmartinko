644aba667ea5ab250a1bc9fc42fc32c8f3c9d437
bmartinko <bennett.martinko@gmail.com>
2018-10-12 01:33:58 -0400

    Wow this is getting late. I should start this earlier... but the Eagles game was on so...

--- src/com/company/Automobile.java
+++ src/com/company/Automobile.java
@@ -4,4 +4,21 @@ package com.company;
  * Created by dpennebacker on 10/9/16.
  */
 public class Automobile {
+    public int miles = 0;
+    public int gallons = 0;
+    public int milesPerGallon = 0;
+
+    public Automobile (int mpg){
+        milesPerGallon = mpg;
+    }
+    public void takeTrip (int mileage){
+        this.miles = miles;
+        gallons = mileage - milesPerGallon;
+    }
+    public void fillUp (int gas){
+        gas += gallons;
+    }
+    public double reportFuel (){
+        return gallons;
+    }
 }
